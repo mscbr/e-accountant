0c426e7d9392de950e43cdca8c3233bb33b84943
mscbr <scibor.maciej@gmail.com>
2019-01-23 23:03:30 +0000

    conditional output initially designed + removed notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,28 +3,49 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 
-import Notifications from './Notifications';
-import ProjectList from '../projects/InvoiceList';
+
+import InvoiceList from '../projects/InvoiceList';
 import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
     
     render() {
-        //console.log(this.props);
-        const { invoices, auth, notifications } = this.props;
+        
+        const { invoices, expenceInvoices ,auth} = this.props;
+        const isLoaded = invoices && expenceInvoices ? true : false;
+        console.log('isLoaded: '+ isLoaded);
+        
         if (!auth.uid) return <Redirect to='/' />
-        return (
-            <div className="dashboard container">
-                <div className="row">
-                    <div className="col s12 m6">
-                        <ProjectList invoices={invoices} />
+        
+        if (isLoaded) {
+            const invoices1 = Object.assign( invoices, expenceInvoices);
+            console.log(invoices1);
+              return (
+                    <div className="dashboard container">
+                        <div className="row">
+                            <div className="col s12 m6">
+                                <div className="card z-depth-0 teal darken-3">
+                                    <span className="card-title white-text">INVOICES</span>
+                                </div>
+
+                                <InvoiceList invoices={invoices1} />
+                            </div>
+                            <div className="col s12 m6">
+                                <div className="card z-depth-0 teal darken-3">
+                                    <span className="card-title white-text">SETTLEMENTS</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col s12 m5 offset-m1">
-                        <Notifications notifications={notifications} />
-                    </div>
+                );
+        } else {
+            return ( 
+                <div className="container center">
+                    <p>Loading invoice...</p>
                 </div>
-            </div>
-        );
+            );
+        }
+        
     }
 }
 
@@ -32,15 +53,17 @@ const mapStateToProps = (state) => {
     //console.log(state);
     return {
         invoices: state.firestore.ordered.invoices,
+        expenceInvoices: state.firestore.ordered.expenceInvoices,
         auth: state.firebase.auth,
-        notifications: state.firestore.ordered.notifications
+        //notifications: state.firestore.ordered.notifications
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([{ collection: 'invoices', orderBy: ['createdAt', 'desc'] /*ADD USERS COLLECTION & PASS UID AS A PROPS TO PROJECT LIST*/},
-        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
+        //{ collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] },
+        { collection: 'expenceInvoices', orderBy: ['createdAt', 'desc'] }
     ])
 )(Dashboard);
 
