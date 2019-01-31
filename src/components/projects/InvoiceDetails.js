3e90631699d59ecd770878d180b7a3c1fddba193
mscbr <scibor.maciej@gmail.com>
2019-01-31 18:09:01 +0100

    changed to class component and data ordering fixed due to new security r

--- src/components/projects/InvoiceDetails.js
+++ src/components/projects/InvoiceDetails.js
@@ -1,38 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase'; 
 import { Redirect } from 'react-router-dom';
 import { compose } from 'redux';
 import moment from 'moment';
 
-const InvoiceDetails = (props) => {
-    const { auth } = props;
-    if (!auth.uid) return <Redirect to='/' />
-    const { invoice } = props;
-    if (invoice) {
-        return (
-            <div className="container section project-details">
-                <div className="card z-depth-0">
-                    <div className="card-content">
-                        <span className="card-title">{invoice.title}</span>
-                        <p>Comments: {invoice.comment}</p>
-                    </div>
-                    <div className="card-action grey lighten-4 grey-text">
-                        <div>Posted by the {invoice.clientName}</div>
-                        <div>{moment(invoice.createdAt.toDate()).calendar()}</div>
+
+class InvoiceDetails extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        const { auth } = this.props;
+        if (!auth.uid) return <Redirect to='/' />
+        console.log(this.props);
+        const { invoice } = this.props;
+        if (invoice) {
+            return (
+                <div className="container section project-details">
+                    <div className="card z-depth-0">
+                        <div className="card-content">
+                            <span className="card-title">{invoice.title}</span>
+                            <p>Comments: {invoice.comment}</p>
+                        </div>
+                        <div className="card-action grey lighten-4 grey-text">
+                            <div>Posted by the {invoice.clientName}</div>
+                            <div>{moment(invoice.createdAt.toDate()).calendar()}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    } else {
-        return ( 
-            <div className="container center">
-                <p>Loading invoice...</p>
-            </div>
-            );
+            )
+        } else {
+            return ( 
+                <div className="container center">
+                    <p>Loading invoice...</p>
+                </div>
+                );
+        }
     }
-  
 }
+
+// const InvoiceDetails = (props) => {
+//     const { auth } = props;
+//     if (!auth.uid) return <Redirect to='/' />
+//     const { invoice } = props;
+//     if (invoice) {
+//         return (
+//             <div className="container section project-details">
+//                 <div className="card z-depth-0">
+//                     <div className="card-content">
+//                         <span className="card-title">{invoice.title}</span>
+//                         <p>Comments: {invoice.comment}</p>
+//                     </div>
+//                     <div className="card-action grey lighten-4 grey-text">
+//                         <div>Posted by the {invoice.clientName}</div>
+//                         <div>{moment(invoice.createdAt.toDate()).calendar()}</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     } else {
+//         return ( 
+//             <div className="container center">
+//                 <p>Loading invoice...</p>
+//             </div>
+//             );
+//     }
+  
+// }
 
 const mapStateToProps = (state, ownProps) => {
     const id = ownProps.match.params.id;
@@ -46,5 +83,9 @@ const mapStateToProps = (state, ownProps) => {
 
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect([{ collection: 'invoices' }])
+    firestoreConnect((props) => [
+        { collection: 'invoices', where: [
+            'userId', '==', props.auth.uid ? props.auth.uid : null
+        ] }
+    ])
 )(InvoiceDetails);
