54153726be202139000d212141b3e1f371507c09
mscbr <scibor.maciej@gmail.com>
2019-01-31 14:03:48 +0100

    dashboard NavLink directs to /dashboard

--- src/components/layout/SignedInLinksClient.js
+++ src/components/layout/SignedInLinksClient.js
@@ -8,7 +8,7 @@ const SignedInLinksClient = (props) => {
 
     return (
         <ul className="right">
-            <li><NavLink to='/'>Dashboard</NavLink></li>
+            <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
             <li><NavLink to='/'>My Documents</NavLink></li>
             <li><NavLink to='/newdocument'>New Document</NavLink></li>
             <li><a onClick={props.signOut}>Log Out</a></li>
