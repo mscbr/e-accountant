3e8a261c17760b2e9df020b6acf023cfbd928d89
mscbr <scibor.maciej@gmail.com>
2019-01-31 18:10:06 +0100

    changed to class component and data ordering fixed due to new security..

--- src/components/projects/InvoiceDetails.js
+++ src/components/projects/InvoiceDetails.js
@@ -42,34 +42,6 @@ class InvoiceDetails extends Component {
     }
 }
 
-// const InvoiceDetails = (props) => {
-//     const { auth } = props;
-//     if (!auth.uid) return <Redirect to='/' />
-//     const { invoice } = props;
-//     if (invoice) {
-//         return (
-//             <div className="container section project-details">
-//                 <div className="card z-depth-0">
-//                     <div className="card-content">
-//                         <span className="card-title">{invoice.title}</span>
-//                         <p>Comments: {invoice.comment}</p>
-//                     </div>
-//                     <div className="card-action grey lighten-4 grey-text">
-//                         <div>Posted by the {invoice.clientName}</div>
-//                         <div>{moment(invoice.createdAt.toDate()).calendar()}</div>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     } else {
-//         return ( 
-//             <div className="container center">
-//                 <p>Loading invoice...</p>
-//             </div>
-//             );
-//     }
-  
-// }
 
 const mapStateToProps = (state, ownProps) => {
     const id = ownProps.match.params.id;
