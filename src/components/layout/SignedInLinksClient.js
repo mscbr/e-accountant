552937e5ddb4eb5296a6e778e3a6bfe31b1634e2
mscbr <scibor.maciej@gmail.com>
2019-01-31 14:13:06 +0100

    'my documents' navlink removed from client navbar

--- src/components/layout/SignedInLinksClient.js
+++ src/components/layout/SignedInLinksClient.js
@@ -9,7 +9,6 @@ const SignedInLinksClient = (props) => {
     return (
         <ul className="right">
             <li><NavLink to='/dashboard'>Dashboard</NavLink></li>
-            <li><NavLink to='/'>My Documents</NavLink></li>
             <li><NavLink to='/newdocument'>New Document</NavLink></li>
             <li><a onClick={props.signOut}>Log Out</a></li>
             <li><NavLink to='/' className='btn btn-floating blue lighten-2'>
