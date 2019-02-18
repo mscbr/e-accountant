94108a75875fa6529eff1dd0e4650f86c224d4a4
mscbr <scibor.maciej@gmail.com>
2019-02-18 21:24:08 +0100

    delete function expanded with clearing out files from storage

--- src/components/projects/InvoiceDetails.js
+++ src/components/projects/InvoiceDetails.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase'; 
+import firebase from "firebase";
 import { Redirect } from 'react-router-dom';
 import { compose } from 'redux';
 import moment from 'moment';
 import DeleteInvoiceDialog from './DeleteInvoiceDialog'
 import { deleteInvoice } from '../../store/actions/invoiceActions';
 
+//invoice files previews styling variables
+const thumbsContainer = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 16
+};
+  
+const thumb = {
+    display: 'inline-flex',
+    borderRadius: 2,
+    border: '1px solid #eaeaea',
+    marginBottom: 8,
+    marginRight: 8,
+    width: 100,
+    height: 100,
+    padding: 4,
+    boxSizing: 'border-box'
+};
 
+const thumbInner = {
+    display: 'flex',
+    minWidth: 0,
+    overflow: 'hidden'
+};
+
+const img = {
+    display: 'block',
+    position: 'relative',
+    width: 'auto',
+    height: '100%',
+    zIndex: 0
+}
 
 class InvoiceDetails extends Component {
     constructor(props) {
@@ -15,8 +48,23 @@ class InvoiceDetails extends Component {
     }
 
     handleDelete = () => {
-        //console.log(this.props.invoiceId);
+        //deleting invoice from database
         this.props.deleteInvoice(this.props.invoiceId);
+        //deleting invoice files from storage
+        const filesName = this.props.invoice.filesName;
+        filesName.map(file => {
+            firebase 
+                .storage()
+                .ref('uploaded')
+                .child(file)
+                .delete()
+                .then(() => {
+                    console.log(file+" succesfully deleted")
+                }).catch((err) => {
+                    console.log(err);
+                })
+        });
+        //redirecting after delete
         this.props.history.push('/dashboard');
     }
 
@@ -25,12 +73,36 @@ class InvoiceDetails extends Component {
         if (!auth.uid) return <Redirect to='/' />
 
         const { invoice } = this.props;
+        console.log(invoice);
+        
+
+        
+
         if (invoice) {
+            const previews = invoice.filesUrl;
+            const thumbs = invoice.filesName.map(fileName => {
+                return (
+                    <div style={thumb} key={fileName}>
+                        <div style={thumbInner}>
+                            <img
+                                src={previews[fileName]}
+                                style={img}
+                                alt={fileName}
+                            />
+                            
+                        </div>
+                    </div>
+                );
+            });
             return (
                 <div className="container section project-details">
                     <div className="card z-depth-0">
                         <div className="card-content">
                             <span className="card-title">{invoice.title}</span>
+                            <aside style={thumbsContainer}>
+                                {thumbs}
+                            </aside>
+                            <p></p>
                             <p>Comments: {invoice.comment}</p>
                             <DeleteInvoiceDialog handleDelete={this.handleDelete} />
                         </div>
