fb37334aa69d97d4d29590da5b3f035cff433603
mscbr <scibor.maciej@gmail.com>
2019-02-26 21:50:07 +0100

    file deletion with clearing out the paths from database developed

--- src/components/projects/UpdateDocument.js
+++ src/components/projects/UpdateDocument.js
@@ -75,7 +75,12 @@ class UpdateDocument extends Component {
         
         
         if (props.invoice && state.docType === '') {
-            return {...props.invoice};
+            return {
+                ...props.invoice,
+                uploadProgress: 100,
+                issueYear: props.invoice.issuePeriod.substring(0,4),
+                issueMonth: props.invoice.issuePeriod.substring(5,8)
+            };
         } else {
             return state;
         }
@@ -86,6 +91,134 @@ class UpdateDocument extends Component {
         this.setState({
             [e.target.id]: e.target.value
         });
+    }
+
+    handleMonthFormat = (e) => {
+        if(e.target.value.length === 1) {
+            e.target.value = "0" + e.target.value;
+        }
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
+    handlePeriodSet = (year, month) => {
+        if(year && month) {
+            this.setState({
+                issuePeriod: year+'-'+month
+            });
+        } else {
+            this.setState({
+                issuePeriod: ''
+            })
+        }
+    }
+
+    //FILE UPLOAD METHOD
+    onDrop = (acceptedFiles, rejectedFiles) => {
+        if(rejectedFiles.length) {
+            const rejectedString = rejectedFiles.map(file => {
+                return "File "+file.name+" was not able to be uploaded.";
+            })
+            alert(rejectedString);
+        }
+        
+        const metadata = {
+            customMetadata: {
+                fileOwner: firebase.auth().currentUser.uid
+            }
+            
+        };
+        
+        acceptedFiles.map(file => {
+            const fileName = new Date().getTime() + '_' +file.name;
+            firebase
+                .storage()
+                .ref('uploaded')
+                .child(fileName)
+                .put(file, metadata)
+                .on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                    (snapshot) => {
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        this.setState({
+                            uploadProgress: progress
+                        });
+                        switch (snapshot.state) {
+                            case firebase.storage.TaskState.PAUSED: // or 'paused'
+                                
+                                break;
+                            case firebase.storage.TaskState.RUNNING: // or 'running'
+                               
+                                break;
+                        }
+                    }, (error) => {
+                        console.log(error);
+                    }, () => {
+                        firebase
+                            .storage()
+                            .ref('uploaded')
+                            .child(fileName)
+                            .getDownloadURL().then((downloadURL) => {
+                                //console.log('File available at', downloadURL);
+                                this.setState({
+                                    filesName: [...this.state.filesName, fileName],
+                                    filesUrl: Object.assign(this.state.filesUrl, {
+                                        [fileName]: downloadURL
+                                    })
+                                    
+                                })
+                            });
+                        }
+                )
+        })
+    }
+    handleDelete = (e) => {
+        e.preventDefault();
+        const filesName = this.state.filesName;
+        filesName.map(file => {
+            firebase
+                .storage()
+                .ref('uploaded')
+                .child(file)
+                .delete()
+                .then(() => {
+                    this.setState({
+                        filesName: [],
+                        filesUrl: {},
+                        uploadProgress: 0
+                    }, () => {
+                        firebase.firestore().collection('invoices').doc(this.props.match.params.id).update({
+                            filesName: [],
+                            filesUrl: {},
+                            uploadProgress: 0
+                        })
+                    })
+
+                    console.log('files deleted successfully!');
+                }).catch((error) => {
+                    console.log(error);
+                });
+        });
+        this.setState({
+            filesName: [],
+            filesUrl: {},
+            uploadProgress: 0
+        })
+        
+    }
+    disableSend = () => {
+        const validationData = {
+            docType: this.state.docType,
+            title: this.state.title,
+            issueYear: this.state.issueYear,
+            issueMonth: this.state.issueMonth,
+            filesName: this.state.filesName,
+            filesUrl: this.state.filesUrl
+        };
+        let values = Object.values(validationData);
+        return values.some(value => value.length < 1)
+        
     }
     
     handleSubmit = (e) => {
@@ -101,15 +234,37 @@ class UpdateDocument extends Component {
         if (!auth.uid) return <Redirect to='/' />
 
         if (this.props.invoice) {
-            const { invoice } = this.props;
-            console.log('render props')
-            console.log(this.state);
+            
+            console.log(this.props);
+            //console.log(this.state);
+            const previews = this.state.filesUrl;
+            const {filesName} = this.state;
+            let uploadProgress = Math.floor(this.state.uploadProgress);
 
+            const thumbs = filesName ? filesName.map(fileName => {
+                return (
+                    <div style={thumb} key={fileName}>
+                        <div style={thumbInner}>
+                            <img
+                                src={previews[fileName]}
+                                style={img}
+                                alt={fileName}
+                            />
+                            
+                        </div>
+                    </div>
+                );
+            }) : console.log('uploading');
+            const deleteButton = this.state.uploadProgress ? (
+                <button className="btn-floating red lighten-1" style={deleteButtonStyle} onClick={(e) => this.handleDelete(e)}>
+                    <i className="small material-icons" style={{marginTop: -7}}>delete</i>
+                </button>
+            ) : null;
             
 
             return (
                 <div>
-                    <div className="container">
+                    <div className="container update-form">
                         <form onSubmit={this.handleSubmit} className="white">
                             <h5 className="grey-text text-darken-3">Update Invoice</h5>
                             <div className="input-field">
@@ -122,7 +277,7 @@ class UpdateDocument extends Component {
                             </div>
                             <div className="input-field">
                                 <label htmlFor="title">Title</label>
-                                <input type="text" id="title" onChange={this.handleChange} /> 
+                                <input type="text" id="title" onChange={this.handleChange} value={this.state.title}/> 
                             </div>
                             <div className="row">
                                 
@@ -148,31 +303,39 @@ class UpdateDocument extends Component {
                                         Period of attached documents issue
                                 </label>
                             </div>
-                            {/* <div className="input-field">
+                            <div className="input-field">
 
                                 <Dropzone accept="image/*,application/pdf"
                                     onDrop={this.onDrop}
                                     maxSize={6291456}
                                 >
                                     {({getRootProps, getInputProps, isDragActive}) => {
-                                        return (
-                                            <div 
-                                                {...getRootProps()}
-                                                className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
-                                            >
-                                                <input {...getInputProps()} />
-                                                {
-                                                    isDragActive ?
-                                                    <p>Drop files here...</p> :
-                                                    <p>Drop your invoice files here (image/pdf)</p>
-                                                }
-                                            </div>
-                                        )
+                                        if (!this.state.filesName.length) {
+                                            return (
+                                                <div 
+                                                    {...getRootProps()}
+                                                    className={classNames('dropzone', {'dropzone--isActive': isDragActive})}
+                                                >
+                                                    <input {...getInputProps()} />
+                                                    {
+                                                        isDragActive ?
+                                                        <p>Drop files here...</p> :
+                                                        <p>Drop your invoice files here (image/pdf)</p>
+                                                    }
+                                                </div>
+                                            )
+                                        } else {
+                                            return (
+                                                <div></div>
+                                            )
+                                        }
+                                        
                                     }}
                                 </Dropzone>
                                 <aside style={thumbsContainer}>
                                     {thumbs} 
                                     {deleteButton}
+                                    <p className="red-text text-lighten1">* files will be deleted permanently & instantly</p>
                                     
                                     <p>{uploadProgress < 100 && uploadProgress > 0 ? `uploading...${uploadProgress}%` : ''}</p>
                                     
@@ -180,11 +343,15 @@ class UpdateDocument extends Component {
                             </div>
                             <div className="input-field">
                                 <label htmlFor="comment">Comment</label>
-                                <textarea id='comment' className='materialize-textarea' onChange={this.handleChange}></textarea>
+                                <textarea id='comment' 
+                                    className='materialize-textarea' 
+                                    onChange={this.handleChange}
+                                    value={this.state.comment}
+                                ></textarea>
                             </div>
                             <div className="input-field">
-                                <button className="btn red lighten-1 z-depth-0" disabled={this.disableSend()}>Send</button>
-                            </div> */}
+                                <button className="btn red lighten-1 z-depth-0" disabled={this.disableSend()}>SAVE</button>
+                            </div>
                         </form>
                     </div>
                 </div>
