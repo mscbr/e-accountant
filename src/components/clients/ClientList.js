d5b6adce18e86f7a7d4916fbe3f6dd34c1c97be9
mscbr <scibor.maciej@gmail.com>
2019-03-10 23:57:00 +0100

    feat: Clients component with list

---
+++ src/components/clients/ClientList.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+
+const clientListStyle = {
+    height: '100%',
+    overflow: 'auto'
+}
+class ClientList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            adress: '',
+            clientName: '',
+            id: '',
+            initials: '',
+            isAcc: false,
+            legalForm: '',
+            nip: '',
+            phoneNumber: '',
+            regon: ''
+        }
+    }
+
+    handleClientPick = (e, client) => {
+        e.preventDefault();
+        this.setState({
+            ...client
+        });
+        console.log(this.state);
+    }
+
+    render() {
+        const {clients} = this.props;
+        return (
+            <div style={clientListStyle}>
+                <div className="row">
+                    <div className="col s5">
+                        {clients.map(client => {
+                            return (
+                                <a key={client.id} href='#' className='black-text' onClick={(e) => this.handleClientPick(e, client)}>
+                                    <div className="card client-list-item">
+                                        <div className="card-content" style={{margin: 1, padding: 2}}>
+                                            <span className="card-title">{client.clientName.substring(0,16)+'...'}</span>
+                                            <p className="grey-text text-darken-1" >ID: {client.id}</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            )
+                        })}
+                    </div>
+                    <div className="col s9">
+                        <div className="card client-details">
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        )
+    }
+    
+}
+
+export default ClientList;
---
+++ src/components/clients/Clients.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import ClientList from './ClientList'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+
+class Clients extends Component {
+    
+    render() {
+        const {users, auth} = this.props;
+        if (!auth.uid) {
+            return <Redirect to='/' />;
+        }
+        if (users) {
+            return (
+                <div className='container'>
+                    <ClientList clients={users} />
+                </div>
+            )
+        } else {
+            return (
+                <div className='container'>
+                    <p>Loading...</p>
+                </div>
+            )
+        }
+       
+    }
+}
+
+const mapStateToProps = (state) => {
+    
+    return {
+        users: state.firestore.ordered.users,
+        auth: state.firebase.auth
+    };
+} 
+
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect((props) => [
+        { collection: 'users', where: [
+            'isAcc', '==', false
+        ] }
+    ])
+)(Clients);
