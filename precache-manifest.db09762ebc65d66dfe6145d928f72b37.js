19a11fa93187481cfbbed22951d13e2df7df9849
mscbr <scibor.maciej@gmail.com>
2019-04-07 23:15:42 +0200

    Updates

---
+++ precache-manifest.db09762ebc65d66dfe6145d928f72b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a0e3390c24024dd1bbc2477cf4b8c61",
+    "url": "/e-accountant/static/media/samson-creative-91091-unsplash.6a0e3390.jpg"
+  },
+  {
+    "revision": "504ffa4e1fc4eee64c9d",
+    "url": "/e-accountant/static/js/runtime~main.504ffa4e.js"
+  },
+  {
+    "revision": "93840eaf4033ba6af62e",
+    "url": "/e-accountant/static/js/main.93840eaf.chunk.js"
+  },
+  {
+    "revision": "c0771a228105ecb5c77e",
+    "url": "/e-accountant/static/js/1.c0771a22.chunk.js"
+  },
+  {
+    "revision": "93840eaf4033ba6af62e",
+    "url": "/e-accountant/static/css/main.782c3414.chunk.css"
+  },
+  {
+    "revision": "1e9cced98929bae8f903b4319dcf74df",
+    "url": "/e-accountant/index.html"
+  }
+];
